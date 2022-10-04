555848f709bfa06cbdca5a356eb018659b5cdebd
whrod <whrodtube@gmail.com>
2022-10-04 17:33:59 +0900

    initial-setting

---
+++ utils/error.js
@@ -0,0 +1,26 @@
+const catchAsync = (func) => {
+  return (req, res, next) => {
+    func(req, res, next).catch(next);
+  };
+};
+
+const globalErrorHandler = (error, req, res, next) => {
+  console.error(error.stack);
+  error.statusCode || 500;
+  res.status(error.statusCode).json({ message: error.message });
+};
+
+//   const affectedRowsErrorHandler = async (result) => {
+//     if (result.affectedRows !== 1) {
+//       const error = new Error('WRONG_INPUT_REQUEST');
+//       error.statusCode = 400;
+
+//       throw error;
+//     }
+//   };
+
+module.exports = {
+  catchAsync,
+  globalErrorHandler,
+  // affectedRowsErrorHandler,
+};
